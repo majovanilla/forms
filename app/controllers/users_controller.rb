e0181f0c1f52b6d7ec61c9ce53e9d4f093dacdfe
majovainilla <maria_reyes@me.com>
2019-11-22 15:44:30 -0600

    Add edit view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,27 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to edit_user_path
+    else
+      render :edit
     end
   end
 
