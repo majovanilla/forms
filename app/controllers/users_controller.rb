ecc9ce93a02e677334e07d2b4703204911f7a710
majovainilla <maria_reyes@me.com>
2019-11-22 12:17:26 -0600

    Add user_params method and change form to send hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,19 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_users_path
     else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:authenticity_token, :username, :email, :password)
   end
 end
