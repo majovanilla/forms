097da7ace201524dd6736be40b6bbca8da492bc7
majovainilla <maria_reyes@me.com>
2019-11-24 14:28:21 -0600

    Add optional short version to User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :username, :email, :password, presence: true
 end
